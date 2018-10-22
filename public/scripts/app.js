5950e88ff7dd2b4817dd48e85d78928f91f4f109
dferrer8 <david.dfg@gmail.com>
2018-10-23 00:14:21 +0200

    Descargado

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
-/* global ReactDOM */
-console.log('Aplicación en ejecución');
+/* global ReactDOM, React */
+console.log('Aplicación en ejecución')
 // ES6
 var user = {
   name: 'Pepe',
   cities: ['Zaragoza', 'Madrid', 'Lérida'],
-  showCities: function showCities() {
-    // ES6 metodo con formato ES6 
-    return this.cities;
+  showCities: function showCities () {
+    // ES6 metodo con formato ES6
+    return this.cities
   }
-};
+}
 
 // pongo llaves porque pongo codigo javascript {city}
 var template = React.createElement(
@@ -21,8 +21,8 @@ var template = React.createElement(
       'p',
       null,
       city
-    );
+    )
   })
-);
-var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+)
+var appRoot = document.getElementById('app')
+ReactDOM.render(template, appRoot)
